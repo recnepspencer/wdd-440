cf417da677ea15b572e93871bff51c552f3e2875
Spencer Hepworth <goldenspencerh@gmail.com>
2024-09-21 17:12:46 -0600

    fixed type any

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 // src/pages/_app.tsx
 import { useRouter } from 'next/router';
 import Layout from '@/app/layout';
-import '../app/globals.css'; // Import global styles
+import '../app/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
